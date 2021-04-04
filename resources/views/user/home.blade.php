68c9052f4a3679e53388df82a17b251c62697add
Harry Chan <tran.ng.song@valuence.inc>
2021-04-04 16:12:07 +0700

    Fix bug

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -19,7 +19,7 @@
         </div>
         <div class="list-category">
             <div class="row">
-@foreach($cates as $item)
+                @foreach($cates as $item)
                 <div class="col m5ths m4 cat-item remove-line-height">
                     <a href="{{ route('categories.show', ['id' => $item->id]) }}">
                         <div class="img-wrapper"
@@ -27,7 +27,7 @@
                         <p>{{ $item->title }}</p>
                     </a>
                 </div>
-@endforeach
+                @endforeach
             </div>
         </div>
         @if(count($flashSale) > 0)
@@ -67,215 +67,13 @@
         @endif
         <div class="products-block">
             <p class="block-title">Sản phẩm mới</p>
-            <div class="products-wrapper">
-    <div class="row products">
-@foreach($proNew as $item)
-        <div class="product col l3 added-wishlist">
-        <?php $img = \App\ProductColor::where('product_id', $item->id)->first();?>
-            <div class="img"  style="background-image: url({{ asset($img ? 'public/' . $img->image : 'public/assets/images/demo/watch.png') }})"></div>
-            <div class="name">
-                <a href="{{ route('products.show', $item->id) }}">
-                    {{ $item->name }}
-                </a>
-            </div>
-            <div class="price">
-                <span class="sale">  {{ $item->price }}đ</span>
-                <span class="origin">  {{ $item->sale_price }}đ</span>
-            </div>
-            <div class="button-wrapper">
-                <a class="modal-trigger" href="#quickview">Xem nhanh</a>
-            </div>
+            @include('layouts.list-product', ['listProduct' => $proNew, 'hasReadMore' => false])
         </div>
-@endforeach
-    </div>
-    <a href="#!" class="view-more">Xem thêm</a>
-</div>
-<!-- Modal Structure -->
-<div id="quickview" class="modal">
-    <div class="modal-content">
-        <a href="#!" class="modal-close close-quickview"><a>
-                <div class="carousel carousel-slider quickview-slider">
-                    <a href="#!" class="previous"></a>
-                    <div class="carousel-item product-img added-wishlist">
-                        <span class="sale-percent">-50%</span>
-                        <img src="{{ asset('public/assets/images/demo/watch.png') }}" />
-                    </div>
-                    <div class="carousel-item product-img">
-                        <img src="{{ asset('public/assets/images/demo/watch.png') }}" />
-                    </div>
-                    <div class="carousel-item product-img">
-                        <img src="{{ asset('public/assets/images/demo/watch.png') }}" />
-                    </div>
-                    <div class="carousel-item product-img">
-                        <img src="{{ asset('public/assets/images/demo/watch.png') }}" />
-                    </div>
-                    <a href="#!" class="next"></a>
-                </div>
-                <div class="product-infos">
-                    <div class="name">Tên của sản phẩm này là</div>
-                    <div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi dignissim
-                        euismod
-                        lectus fermentum mollis lorem nec aliquam urna. Id fames vestibulum quis nam pharetra, id magna
-                        nibh
-                        cras. At fames id arcu elit tristique commodo eu, integer eget. Varius purus, tortor euismod
-                        dolor in.
-                        Sagittis, vulputate consectetur pellentesque quis facilisis accumsan</div>
-                    <div class="list-tags">
-                        <span class="tag hang-moi"></span>
-                        <span class="tag ban-chay"></span>
-                        <span class="tag giam-gia"></span>
-                    </div>
-                    <div class="price">
-                        <div class="origin">36.400.000đ</div>
-                        <div class="sale">18.200.000đ <span>Giảm 50%</span></div>
-                    </div>
-                    <div class="status-wrapper">
-                        <div class="status-label">Tình trạng</div>
-                        <div class="list-status">
-                            <span class="status het-hang"></span>
-                            <span class="status dat-truoc"></span>
-                            <span class="status con-hang"></span>
-                        </div>
-                    </div>
-                    <div class="colors-wrapper">
-                        <div class="color-label">Màu sắc</div>
-                        <div class="colors">
-                            <span class="color">Xanh lá</span>
-                            <span class="color">Xanh da trời</span>
-                            <span class="color">Tím</span>
-                        </div>
-                    </div>
-                    <div class="sizes-wrapper">
-                        <div class="size-label">Kích thước<a href="#!" target="_blank">(Hướng dẫn chọn size)</a></div>
-                        <div class="sizes">
-                            <span class="size">XS</span>
-                            <span class="size">S</span>
-                            <span class="size">M</span>
-                            <span class="size">L</span>
-                            <span class="size">XL</span>
-                        </div>
-                    </div>
-                    <div class="pre-order-block">
-                        <div class="quantity-input">
-                            <span class="decrease">-</span>
-                            <input type="number" class="quantity" value="0" />
-                            <span class="increase">+</span>
-                        </div>
-                        <a href="#!" class="add-to-card-btn">Thêm vào giỏ hàng</a>
-                        <a href="{{ route('user.cart') }}" class="buy-now-btn">Mua ngay</a>
-                    </div>
-                </div>
-    </div>
-</div>
-</div>
-<div class="products-block">
-    <p class="block-title">Sản phẩm bán chạy</p>
-    <div class="products-wrapper">
-        <div class="row products">
-            @foreach($proTopSold as $item)
-                <div class="product col l3 added-wishlist">
-                <?php $img = \App\ProductColor::where('product_id', $item->id)->first();?>
-                    <div class="img"
-                    style="background-image: url({{ asset($img ? 'public/' . $img->image : 'public/assets/images/demo/watch.png') }})">
-                    </div>
-                    <div class="name">
-                        <a href="{{ route('products.show', $item->id) }}">
-                            {{ $item->name }}
-                        </a>
-                    </div>
-                    <div class="price">
-                        <span class="sale"> {{ $item->price }}đ</span>
-                        <span class="origin"> {{ $item->sale_price }}đ</span>
-                    </div>
-                    <div class="button-wrapper">
-                        <a class="modal-trigger" href="#quickview">Xem nhanh</a>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-        <a href="#!" class="view-more">Xem thêm</a>
-    </div>
-    <!-- Modal Structure -->
-    <div id="quickview" class="modal">
-        <div class="modal-content">
-            <a href="#!" class="modal-close close-quickview"><a>
-                    <div class="carousel carousel-slider quickview-slider">
-                        <a href="#!" class="previous"></a>
-                        <div class="carousel-item product-img added-wishlist">
-                            <span class="sale-percent">-50%</span>
-                            <img src="{{ asset('public/assets/images/demo/watch.png') }}" />
-                        </div>
-                        <div class="carousel-item product-img">
-                            <img src="{{ asset('public/assets/images/demo/watch.png') }}" />
-                        </div>
-                        <div class="carousel-item product-img">
-                            <img src="{{ asset('public/assets/images/demo/watch.png') }}" />
-                        </div>
-                        <div class="carousel-item product-img">
-                            <img src="{{ asset('public/assets/images/demo/watch.png') }}" />
-                        </div>
-                        <a href="#!" class="next"></a>
-                    </div>
-                    <div class="product-infos">
-                        <div class="name">Tên của sản phẩm này là</div>
-                        <div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mi dignissim
-                            euismod
-                            lectus fermentum mollis lorem nec aliquam urna. Id fames vestibulum quis nam pharetra, id
-                            magna
-                            nibh
-                            cras. At fames id arcu elit tristique commodo eu, integer eget. Varius purus, tortor euismod
-                            dolor in.
-                            Sagittis, vulputate consectetur pellentesque quis facilisis accumsan</div>
-                        <div class="list-tags">
-                            <span class="tag hang-moi"></span>
-                            <span class="tag ban-chay"></span>
-                            <span class="tag giam-gia"></span>
-                        </div>
-                        <div class="price">
-                            <div class="origin">36.400.000đ</div>
-                            <div class="sale">18.200.000đ <span>Giảm 50%</span></div>
-                        </div>
-                        <div class="status-wrapper">
-                            <div class="status-label">Tình trạng</div>
-                            <div class="list-status">
-                                <span class="status het-hang"></span>
-                                <span class="status dat-truoc"></span>
-                                <span class="status con-hang"></span>
-                            </div>
-                        </div>
-                        <div class="colors-wrapper">
-                            <div class="color-label">Màu sắc</div>
-                            <div class="colors">
-                                <span class="color">Xanh lá</span>
-                                <span class="color">Xanh da trời</span>
-                                <span class="color">Tím</span>
-                            </div>
-                        </div>
-                        <div class="sizes-wrapper">
-                            <div class="size-label">Kích thước<a href="#!" target="_blank">(Hướng dẫn chọn size)</a>
-                            </div>
-                            <div class="sizes">
-                                <span class="size">XS</span>
-                                <span class="size">S</span>
-                                <span class="size">M</span>
-                                <span class="size">L</span>
-                                <span class="size">XL</span>
-                            </div>
-                        </div>
-                        <div class="pre-order-block">
-                            <div class="quantity-input">
-                                <span class="decrease">-</span>
-                                <input type="number" class="quantity" value="0" />
-                                <span class="increase">+</span>
-                            </div>
-                            <a href="#!" class="add-to-card-btn">Thêm vào giỏ hàng</a>
-                            <a href="{{ route('user.cart') }}" class="buy-now-btn">Mua ngay</a>
-                        </div>
-                    </div>
+        <div class="products-block">
+            <p class="block-title">Sản phẩm bán chạy</p>
+            @include('layouts.list-product', ['listProduct' => $proTopSold, 'hasReadMore' => false])
         </div>
     </div>
-</div>
-</div>
 </div>
 <div class="ipk-container partners-container">
     <div class="partners">
--- resources/views/user/product-details.blade.php
+++ resources/views/user/product-details.blade.php
@@ -109,12 +109,12 @@
             </div>
             <div class="pre-order-block">
                 <div class="quantity-input">
-                    <span class="decrease">-</span>
-                    <input type="number" class="quantity" value="1" id="quantity" />
-                    <span class="increase">+</span>
+                    <span class="decrease-detail">-</span>
+                    <input type="number" class="quantity" value="1" id="quantity-detail" />
+                    <span class="increase-detail">+</span>
                 </div>
-                <a href="#!" class="add-to-card-btn">Thêm vào giỏ hàng</a>
-                <a href="#!" class="buy-now-btn" id="buy-now-btn">Mua ngay</a>
+                <a href="#!" class="add-to-card-btn-detail">Thêm vào giỏ hàng</a>
+                <a href="#!" class="buy-now-btn" id="buy-now-btn-detail">Mua ngay</a>
             </div>
         </div>
     </div>
@@ -164,9 +164,80 @@
 @endsection
 
 @section('scripts')
-<script src="{{ asset('public/assets/scripts/iphukien/user/product-details.js') }}"></script>
 <script src="{{ asset('public/assets/scripts/iphukien/user/list-product.js') }}"></script>
 <script>
+$(document).ready(function () {
+    var elems = document.querySelectorAll('#list-image-popup');
+    M.Modal.init(elems, {
+        'onOpenEnd': calcListThumbsWidth
+    });
+
+    function calcListThumbsWidth() {
+        let w = $('.thumbs-popup span').length * 122 + ($('.thumbs-popup span').length - 1) * 18;
+        $('.thumbs-popup')[0].style.width = w + 'px';
+    }
+
+});
+$(document).on("click", ".list-thumb-wrapper .up", function () {
+    if ($(".thumbs")[0].offsetHeight - $(".thumbs span:not(.hide)")[0].offsetHeight <= 470) return;
+    $.each($(".thumbs span"), function (index, value) {
+        if (!value.classList.contains('hide')) {
+            value.classList.add('hide');
+            return false;
+        }
+    });
+});
+$(document).on("click", ".list-thumb-wrapper .down", function () {
+    if ($(".thumbs span.hide").length == 0) return;
+    $(".thumbs span.hide")[$(".thumbs span.hide").length - 1].classList.remove('hide');
+});
+$(document).on("click", ".color", function () {
+    $('.color').removeClass('active');
+    $(this).addClass('active');
+    $('.main-image')[0].style.backgroundImage = "url(" + $(this).data('img') + ")";
+});
+$(document).on("click", ".size", function () {
+    $('.size').removeClass('active');
+    $(this).addClass('active');
+});
+$(document).on("click", ".decrease-detail", function () {
+    if ($('.quantity').val() == 0) return;
+    $('.quantity').val(parseInt($('.quantity').val()) - 1)
+});
+$(document).on("click", ".increase-detail", function () {
+    $('.quantity').val(parseInt($('.quantity').val()) + 1)
+});
+$(document).on("click", ".list-thumbs-popup .arrow-left", function () {
+    if ($(".thumbs-popup span.hide").length == 0) return;
+    $(".thumbs-popup span.hide")[$(".thumbs-popup span.hide").length - 1].classList.remove('hide');
+    let w = $('.thumbs-popup span:not(.hide)').length * 122 + ($('.thumbs-popup span:not(.hide)').length - 1) * 18;
+    $('.thumbs-popup')[0].style.width = w + 'px';
+    $('.thumbs-popup span:not(.hide)')[1].style.marginLeft = '18px';
+});
+$(document).on("click", ".list-thumbs-popup .arrow-right", function () {
+    if ($(".thumbs-popup")[0].offsetWidth <= $(".thumbs-wrapper-popup")[0].offsetWidth) return;
+    $.each($(".thumbs-popup span"), function (index, value) {
+        if (!value.classList.contains('hide')) {
+            value.classList.add('hide');
+            // reset width
+            let w = $('.thumbs-popup span:not(.hide)').length * 122 + ($('.thumbs-popup span:not(.hide)').length - 1) * 18;
+            $('.thumbs-popup')[0].style.width = w + 'px';
+            $('.thumbs-popup span:not(.hide)')[0].style.marginLeft = '0'
+            // return
+            return false;
+        }
+    });
+});
+$(document).on("click", ".thumbs-popup span", function () {
+    $('.main-image-popup')[0].style.backgroundImage = "url(" + $(this).data('img') + ")";
+});
+$(document).on("click", ".thumbs span", function () {
+    $('.main-image')[0].style.backgroundImage = "url(" + $(this).data('img') + ")";
+});
+$(document).on("click", ".custom-fb-share-button", function () {
+    $('.fb-share-button').trigger( "click" );
+});
+
 function updateCart() {
     let listColorElement = $(".colors .color.active");
     if(listColorElement.length == 0) {
@@ -190,7 +261,7 @@ function updateCart() {
     let choosenSize = listSizeElement[0].dataset.sizeid;
     let choosenSizeName = listSizeElement[0].dataset.sizename;
 
-    let quantity = $("#quantity").val() == 0 ? 1 : $("#quantity").val();
+    let quantity = $("#quantity-detail").val() == 0 ? 1 : $("#quantity-detail").val();
 
     let cart = localStorage.getItem('ipk_cart') ? JSON.parse(localStorage.getItem('ipk_cart')) : {};
     // console.log(cart);
@@ -212,12 +283,12 @@ function updateCart() {
     localStorage.setItem('ipk_cart',  JSON.stringify(cart));
     return true;
 }
-$(document).on("click","#buy-now-btn",function() {
+$(document).on("click","#buy-now-btn-detail",function() {
     if(!updateCart()) return;
 
     window.location.href = "{{ route('user.cart') }}";
 });
-$(document).on("click",".add-to-card-btn",function() {
+$(document).on("click",".add-to-card-btn-detail",function() {
     let updateRes = updateCart();
     if(updateRes) {
         M.toast({
