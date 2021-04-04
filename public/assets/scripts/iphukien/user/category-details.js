a853d88073cf7705625b7a6907e49aa0760177f3
NAD <lua.solution@gmail.com>
2021-04-04 16:06:02 +0700

    recommit

--- public/assets/scripts/iphukien/user/category-details.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function () {
-    $('.filter-slide-out').sidenav();
-});
--- resources/ajax/location.blade.php
+++
@@ -1,3 +0,0 @@
-@foreach ($list_data as $data)
-    <option value="{{ $data->id }}">{{ $data->name }}</option>
-@endforeach
