f1d50414c8f2f728d62799e7a366e696e590b56d
Harry Chan <tran.ng.song@valuence.inc>
2021-04-24 03:17:59 +0700

    u

--- app/Http/Controllers/User/CategoryController.php
+++ app/Http/Controllers/User/CategoryController.php
@@ -17,27 +17,27 @@ class CategoryController extends Controller
     public function show(Request $request, $id)
     {
         if ($id == -1) {
-            $this->data['listProduct'] = Product::orderBy('id', 'DESC')->paginate(8);
+            $this->data['listProduct'] = Product::orderBy('id', 'DESC')->whereNull('parent_id')->paginate(8);
         } else if ($request->sort) {
             switch ($request->sort) {
                 case "az":
-                    $this->data['listProduct'] = Product::where('category_id', $id)->orderBy('name', 'ASC')->paginate(8);
+                    $this->data['listProduct'] = Product::where('category_id', $id)->whereNull('parent_id')->orderBy('name', 'ASC')->paginate(8);
                     break;
                 case "za":
-                    $this->data['listProduct'] = Product::where('category_id', $id)->orderBy('name', 'DESC')->paginate(8);
+                    $this->data['listProduct'] = Product::where('category_id', $id)->whereNull('parent_id')->orderBy('name', 'DESC')->paginate(8);
                     break;
                 case "pasc":
-                    $this->data['listProduct'] = Product::where('category_id', $id)->orderBy('price', 'ASC')->orderBy('sale_price', 'ASC')->paginate(8);
+                    $this->data['listProduct'] = Product::where('category_id', $id)->whereNull('parent_id')->orderBy('price', 'ASC')->orderBy('sale_price', 'ASC')->paginate(8);
                     break;
                 case "pdesc":
-                    $this->data['listProduct'] = Product::where('category_id', $id)->orderBy('price', 'DESC')->orderBy('sale_price', 'DESC')->paginate(8);
+                    $this->data['listProduct'] = Product::where('category_id', $id)->whereNull('parent_id')->orderBy('price', 'DESC')->orderBy('sale_price', 'DESC')->paginate(8);
                     break;
                 default:
-                    $this->data['listProduct'] = Product::where('category_id', $id)->orderBy('id', 'DESC')->paginate(8);
+                    $this->data['listProduct'] = Product::where('category_id', $id)->whereNull('parent_id')->orderBy('id', 'DESC')->paginate(8);
             }
 
         } else {
-            $this->data['listProduct'] = Product::where('category_id', $id)->orderBy('id', 'DESC')->paginate(8);
+            $this->data['listProduct'] = Product::where('category_id', $id)->whereNull('parent_id')->orderBy('id', 'DESC')->paginate(8);
         }
 
         if ($id == -1) {
@@ -72,11 +72,13 @@ class CategoryController extends Controller
             $product = DB::table('products')
                 ->join('product_color', 'products.id', 'product_color.product_id')
                 ->join('colors', 'colors.id', 'product_color.color_id')
+                ->whereNull('products.parent_id')
                 ->whereIn('colors.id', json_decode($data['colors']));
         }
 
         if (count(json_decode($data['tags'])) > 0) {
             $product = DB::table('products')
+                ->whereNull('products.parent_id')
                 ->join('tags', 'tags.id', '=', 'products.tag_id')
                 ->whereIn('products.tag_id', json_decode($data['tags']));
         }
@@ -85,24 +87,27 @@ class CategoryController extends Controller
             $product = DB::table('products')
                 ->join('product_size', 'products.id', 'product_size.product_id')
                 ->join('sizes', 'sizes.id', 'product_size.size_id')
+                ->whereNull('products.parent_id')
                 ->whereIn('sizes.id', json_decode($data['sizes']));
         }
 
         if (count(json_decode($data['trademarks'])) > 0) {
             $product = DB::table('products')
                 ->join('trademarks', 'trademarks.id', 'products.trademark_id')
+                ->whereNull('products.parent_id')
                 ->whereIn('trademarks.id', json_decode($data['trademarks']));
         }
 
         if (count(json_decode($data['prices'])) > 0) {
             $product = DB::table('products')
+                ->whereNull('products.parent_id')
                 ->whereBetween('price', json_decode($data['prices']));
         }
 
         if (count(json_decode($data['tags'])) === 0 && count(json_decode($data['trademarks'])) === 0 && count(json_decode($data['sizes'])) === 0 && count(json_decode($data['colors'])) === 0 && count(json_decode($data['prices'])) === 0) {
-            $product = Product::where('category_id', $id);
+            $product = Product::where('category_id', $id)->whereNull('products.parent_id');
         } else {
-            $product = $product->where('category_id', $id);
+            $product = $product->where('category_id', $id)->whereNull('products.parent_id');
         }
 
         $listProduct = $product
