409aed45a7a593ca6ec31cce75c741043da84e67
NAD <lua.solution@gmail.com>
2021-05-22 17:34:52 +0700

    x

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,6 +42,40 @@
 </head>
 
 <body>
+<!-- Messenger Plugin chat Code -->
+<div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v10.0'
+          });
+        };
+
+        (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+      </script>
+
+      <!-- Your Plugin chat code -->
+      <div class="fb-customerchat"
+        attribution="biz_inbox"
+        page_id="1447115355541411">
+      </div>
+
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FY9QF10LML"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FY9QF10LML');
+</script>
 @yield('fb-sdk')
 @yield('header')
 @yield('content')
